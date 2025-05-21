21d1ca7ded62c28b4e1e9e7458ed03d4f9ee7633
Jenish <jpat534@gmail.com>
2025-05-21 18:51:58 +1000

    Added index and layout files for the auth route and index route

---
+++ client/app/(index)/index.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "@/components/ThemedText";
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+    return(
+        <View> 
+            <ThemedText type="title" >Home</ThemedText>
+        </View>
+    )
+}
