654b62566622bb40964192371f650d3b84e4fa83
Jenish <jpat534@gmail.com>
2025-05-21 20:37:54 +1000

    Added a function called createTokenCache

---
+++ client/cache.ts
@@ -0,0 +1,25 @@
+import * as SecureStore from 'expo-secure-store';
+import { TokenCache } from '@clerk/clerk-expo';
+import { Platform } from 'react-native';
+
+const createTokenCache = (): TokenCache => {
+    return{
+        getToken: async (key: string) => {
+            try {
+                const item = await SecureStore.getItemAsync(key);
+                if (item) {
+                    console.log(`${key} was used to get token`);
+                } else {
+                    console.log(`${key} was not found`);
+                }
+                return item;
+            } catch (error) {
+                console.error(`Error getting token for ${key}:`, error);
+            }
+            
+        },
+        saveToken: async (key: string, token: string) => {
+            return
+        }
+    }
+};
