d93e3f7e55661d49afc913e081e7770e2cea07d3
Jenish <jpat534@gmail.com>
2025-05-22 18:03:22 +1000

    imported useAuth and setup a process to handle loading and whether a user is signed in

--- client/app/(auth)/_layout.tsx
+++ client/app/(auth)/_layout.tsx
@@ -1,6 +1,11 @@
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
 
 export default function AuthRoutesLayout() {
+    const { isLoaded, isSignedIn } = useAuth(); 
+
+    if (!isLoaded) return null;
+    if (isSignedIn) return <Redirect href={("/(index)")} />
     return (
         <Stack >
             <Stack.Screen name="index" />
