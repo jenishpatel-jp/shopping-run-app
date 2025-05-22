24c79ba8bb1daa5ff4d10905f4432273ee6a9316
Jenish <jpat534@gmail.com>
2025-05-22 18:06:08 +1000

    Added the reset and signup page to the layout

--- client/app/(auth)/_layout.tsx
+++ client/app/(auth)/_layout.tsx
@@ -8,7 +8,9 @@ export default function AuthRoutesLayout() {
     if (isSignedIn) return <Redirect href={("/(index)")} />
     return (
         <Stack >
-            <Stack.Screen name="index" />
+            <Stack.Screen name="index" options={{headerTitle: "Sign In"}} />
+            <Stack.Screen name="sign-up" options={{headerTitle: "Sign Up"}} />
+            <Stack.Screen name="reset-password" options={{headerTitle: "Reset Password"}} />
         </Stack>
     )
 }
