7cd98b6c93823081665f6fbff7eed08701f5cdc5
Jenish <jpat534@gmail.com>
2025-05-22 18:36:27 +1000

    Added sizeStyle and variantStyles

--- client/components/ui/button.tsx
+++ client/components/ui/button.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { ViewStyle, TextStyle } from "react-native";
+import React, { use } from "react";
+import { ViewStyle, TextStyle, useColorScheme } from "react-native";
 
 type ButtonVariant = "filled" | "outlined" | "ghost";
 type ButtonSize = "sm" | "md" | "lg";
@@ -15,6 +15,35 @@ interface ButtonProps {
     textStyle?: TextStyle;
 }
 
-export const Button = () => {
+export const Button: React.FC<ButtonProps> = ({
+    onPress,
+    variant = "filled",
+    size = "md",
+    disabled = false,
+    loading = false,
+    children,
+    style,
+    textStyle,
+}) => {
+    const colorScheme = useColorScheme();
+    const isDark = colorScheme === "dark";
 
-}
+    const sizeStyles: Record<
+        ButtonSize,
+        { height: number; fontsize: number, padding: number }
+    > = {
+        sm: { height: 36, fontsize: 14, padding: 12 },
+        md: { height: 44, fontsize: 16, padding: 16 },
+        lg: { height: 55, fontsize: 18, padding: 20 },
+    };
+
+    const getVariantStyles = () => {
+        const baseStyle = {
+            borderRadius: 12,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+        };
+    };
+
+};
