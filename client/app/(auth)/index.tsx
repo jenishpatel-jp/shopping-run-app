0b5208623d4bd031123cdd79786e55fe8df0ae94
Jenish <jpat534@gmail.com>
2025-05-22 18:10:42 +1000

    Added useStates for SignInScreen

--- client/app/(auth)/index.tsx
+++ client/app/(auth)/index.tsx
@@ -1,10 +1,21 @@
 import { ThemedText } from "@/components/ThemedText";
-import { View, Text } from "react-native";
+import { useSignIn } from "@clerk/clerk-expo";
+import { View, Text, Button} from "react-native";
+import { useRouter, Link } from "expo-router";
+import { useState } from "react";
 
 export default function SignInScreen() {
+    const { signIn, setActive, isLoaded } = useSignIn();
+    const router = useRouter();
+
+    const [emailAddress, setEmailAddress] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
+
     return(
         <View> 
             <ThemedText type="title" >Sign In</ThemedText>
+            <Link href={"/sign-up"}></Link>
         </View>
     )
 }
