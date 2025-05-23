88e08fe9dd3c2a3f8e80e72b65ed3911afbdcd47
Jenish <jpat534@gmail.com>
2025-05-23 18:32:57 +1000

    Added text inputs for password as well as links to forgot password nad registering

--- client/app/(auth)/index.tsx
+++ client/app/(auth)/index.tsx
@@ -1,6 +1,6 @@
 import { ThemedText } from "@/components/ThemedText";
 import { useSignIn } from "@clerk/clerk-expo";
-import { View, Text} from "react-native";
+import { View, Text, StyleSheet} from "react-native";
 import { useRouter, Link } from "expo-router";
 import { useState } from "react";
 import Button from "@/components/ui/button";
@@ -16,10 +16,15 @@ export default function SignInScreen() {
     const [password, setPassword] = useState<string>("");
     const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
 
+    const onSignInPress = () => {
+
+    }
+
     return(
         
         <SafeAreaView>
             <BodyScrollView
+                style={styles.contentContainer}
             >
                 <TextInput  
                     label="Email" 
@@ -28,9 +33,46 @@ export default function SignInScreen() {
                     onChangeText={setEmailAddress}
                     autoCapitalize="none"
                     />
-                <Button>Hello</Button>
+                <TextInput 
+                    label="Password" 
+                    value={password}
+                    placeholder="Enter password"
+                    autoCapitalize="none"
+                    secureTextEntry={true}
+                    onChangeText={(password) => setPassword(password)}
+
+                />
+                <Button
+                    onPress={onSignInPress}
+                    loading={isSignedIn}
+                    disabled={!emailAddress || !password || isSignedIn}>
+                        Sign In
+                </Button>
+
+                <View style={styles.dontHaveAccount}>
+                    <ThemedText> Don't have an account? </ThemedText>
+                    <Button onPress={()=> router.push("/sign-up")} variant="ghost" > Sign Up</Button>
+                
+                </View>
+
+                <View style={styles.dontHaveAccount}>
+                    <ThemedText> Forgot Password? </ThemedText>
+                    <Button onPress={()=> router.push("/reset-password")} variant="ghost" > Reset Password</Button>
+                
+                </View>
+
             </BodyScrollView>
         </SafeAreaView>
 
     )
 }
+
+const styles = StyleSheet.create({
+    contentContainer: {                    
+        padding: 16,
+    },
+    dontHaveAccount: {
+        marginTop: 16,
+        alignItems: "center",
+    },
+})
