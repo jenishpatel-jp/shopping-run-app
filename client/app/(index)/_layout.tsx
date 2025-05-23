221c67466531358d6a4892353e12fa8f3364564a
Jenish <jpat534@gmail.com>
2025-05-23 17:32:52 +1000

    Added screen options in the index layout

--- client/app/(index)/_layout.tsx
+++ client/app/(index)/_layout.tsx
@@ -2,7 +2,23 @@ import { Stack } from "expo-router";
 
 export default function HomeRoutesLayout() {
     return (
-        <Stack >
+        <Stack 
+            screenOptions={{
+                ...(process.env.EXPO_OS !== "ios"
+                    ? {} 
+                    : {
+                        headerLargeTitle: true,
+                        headerTransparent: true,
+                        headerBlurEffect: "systemChromeMaterial",
+                        headerLargeTitleShadowVisible: false,
+                        headerShadowVisible: true,
+                        headerLargeStyle: {
+                            backgroundColor: "transparent",
+                        },
+                    }),
+
+                }}
+        >
             <Stack.Screen name="index" />
         </Stack>
     )
