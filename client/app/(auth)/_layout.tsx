84bd668dd519984bf934bd16425135e7cdef91a1
Jenish <jpat534@gmail.com>
2025-05-23 17:25:55 +1000

    added screenOptions

--- client/app/(auth)/_layout.tsx
+++ client/app/(auth)/_layout.tsx
@@ -7,7 +7,24 @@ export default function AuthRoutesLayout() {
     if (!isLoaded) return null;
     if (isSignedIn) return <Redirect href={("/(index)")} />
     return (
-        <Stack >
+        <Stack 
+            screenOptions={{
+                ...(process.env.EXPO_OS !== "ios"
+                    ? {} 
+                    : {
+                        headerLargeTitle: true,
+                        headerTransparent: true,
+                        headerBlurEffect: "systemChromeMaterial",
+                        headerLargeTitleShadowVisible: false,
+                        headerShadowVisible: true,
+                        headerLargeStyle: {
+                            backgroundColor: "transparent",
+                        },
+                    }),
+
+                }}
+        
+        >
             <Stack.Screen name="index" options={{headerTitle: "Sign In"}} />
             <Stack.Screen name="sign-up" options={{headerTitle: "Sign Up"}} />
             <Stack.Screen name="reset-password" options={{headerTitle: "Reset Password"}} />
