0df0bb537c33fa25907250a3ac5878ccca939552
Jenish <jpat534@gmail.com>
2025-05-24 14:43:43 +1000

    imported the resetPassword component

--- client/app/(auth)/reset-password.tsx
+++ client/app/(auth)/reset-password.tsx
@@ -1,33 +1,118 @@
+import * as React from "react";
+import { useRouter } from "expo-router";
 import { ThemedText } from "@/components/ThemedText";
 import { BodyScrollView } from "@/components/ui/BodyScrollView";
-import TextInput from "@/components/ui/text-input";
-import { View, Text, StyleSheet, Platform } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import Button from "@/components/ui/button";
-import { useRouter } from "expo-router";
+import TextInput from "@/components/ui/text-input";
+import { isClerkAPIResponseError, useSignIn } from "@clerk/clerk-expo";
+import { ClerkAPIError } from "@clerk/types";
+import { StyleSheet, Platform } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const router = useRouter();
+export default function ResetPassword() {
+  const { isLoaded, signIn, setActive } = useSignIn();
+  const router = useRouter();
 
-export default function ResetScreen() {
-    return(
-        <SafeAreaView>
-            <BodyScrollView
-                style={styles.contentContainer}
-            >
-          
+  const [emailAddress, setEmailAddress] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [pendingVerification, setPendingVerification] = React.useState(false);
+  const [code, setCode] = React.useState("");
+  const [errors, setErrors] = React.useState<ClerkAPIError[]>([]);
 
-            </BodyScrollView>
-        </SafeAreaView>
-    )
-}
+  const onResetPasswordPress = React.useCallback(async () => {
+    if (!isLoaded) return;
+    setErrors([]);
+
+    try {
+      await signIn.create({
+        strategy: "reset_password_email_code",
+        identifier: emailAddress,
+      });
+
+      setPendingVerification(true);
+    } catch (err) {
+      if (isClerkAPIResponseError(err)) setErrors(err.errors);
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }, [isLoaded, emailAddress, signIn]);
+
+  const onVerifyPress = React.useCallback(async () => {
+    if (!isLoaded) return;
+
+    try {
+      const signInAttempt = await signIn.attemptFirstFactor({
+        strategy: "reset_password_email_code",
+        code,
+        password,
+      });
+
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+        router.replace("/");
+      } else {
+        console.error(JSON.stringify(signInAttempt, null, 2));
+      }
+    } catch (err) {
+      if (isClerkAPIResponseError(err)) setErrors(err.errors);
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }, [isLoaded, code, password, signIn, setActive, router]);
+
+  if (pendingVerification) {
+    return (
+      <BodyScrollView contentContainerStyle={styles.contentContainer}>
+        <TextInput
+          value={code}
+          label={`Enter the verification code we sent to ${emailAddress}`}
+          placeholder="Enter your verification code"
+          onChangeText={setCode}
+        />
+        <TextInput
+          value={password}
+          label="Enter your new password"
+          placeholder="Enter your new password"
+          secureTextEntry
+          onChangeText={setPassword}
+        />
+        {errors.map((error) => (
+          <ThemedText key={error.longMessage} style={{ color: "red" }}>
+            {error.longMessage}
+          </ThemedText>
+        ))}
+        <Button onPress={onVerifyPress} disabled={!code || !password}>
+          Reset password
+        </Button>
+      </BodyScrollView>
+    );
+  }
+
+  return (
+    <SafeAreaView>
+        <BodyScrollView contentContainerStyle={styles.contentContainer}>
+        <TextInput
+            autoCapitalize="none"
+            value={emailAddress}
+            placeholder="Enter email"
+            keyboardType="email-address"
+            onChangeText={setEmailAddress}
+        />
+        <Button onPress={onResetPasswordPress} disabled={!emailAddress}>
+            Continue
+        </Button>
+        {errors.map((error) => (
+            <ThemedText key={error.longMessage} style={{ color: "red" }}>
+            {error.longMessage}
+            </ThemedText>
+        ))}
+        </BodyScrollView>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
     contentContainer: {                    
         padding: 16,
         paddingTop: Platform.OS === "android" ? 60 : 16,
     },
-    dontHaveAccount: {
-        marginTop: 16,
-        alignItems: "center",
-    },
+
 })
