d5069da7b20b831f64e9a54998bff62818ec9185
Jenish <jpat534@gmail.com>
2025-05-26 18:43:13 +1000

    Added themed text as well as created a stylesheet

--- client/app/(index)/list/new/index.tsx
+++ client/app/(index)/list/new/index.tsx
@@ -1,10 +1,25 @@
 import { ThemedText } from "@/components/ThemedText";
 import { BodyScrollView } from "@/components/ui/BodyScrollView";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StyleSheet, Platform } from "react-native";
 
 export default function NewListScreen() {
     return (
-        < BodyScrollView >
-            <ThemedText> Create List </ThemedText>
+        < BodyScrollView  style={styles.contentContainer} >
+            <ThemedText type="subtitle" style={styles.subtitle} > Better Together </ThemedText>
+            <ThemedText type="defaultSemiBold" > Create shared shopping lists and collaborate in real time with family and friends </ThemedText>
         </BodyScrollView>
+    
     )
 }
+
+const styles = StyleSheet.create({
+    contentContainer: {                    
+        padding: 16,
+        paddingTop: Platform.OS === "android" ? 60 : 16,
+    },
+    subtitle: {
+
+    }
+
+})
