dccb1ebce38aa0f803de3e94dd4bc78b7dcd352b
Jenish <jpat534@gmail.com>
2025-05-26 19:24:32 +1000

    Added code to make the background a Random Colour

--- client/app/(index)/list/new/index.tsx
+++ client/app/(index)/list/new/index.tsx
@@ -3,15 +3,23 @@ import { BodyScrollView } from "@/components/ui/BodyScrollView";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StyleSheet, Platform } from "react-native";
 import IconCircle from "@/components/IconCircle";
-import { emojies } from "@/constants/Colors";
+import { backgroundColors, emojies } from "@/constants/Colors";
+import { useMemo } from "react";
 
 export default function NewListScreen() {
 
-    const randomEmoji = emojies[Math.floor(Math.random() * emojies.length)];
+    const randomEmoji = useMemo(() => emojies[Math.floor(Math.random() * emojies.length)], []);
+
+    const randomColour = useMemo(() => backgroundColors[Math.floor(Math.random() * backgroundColors.length)], []);
     
     return (
         < BodyScrollView  style={styles.contentContainer} >
-            <IconCircle emoji={randomEmoji} size={60} style={styles.iconCircle}/>
+            <IconCircle 
+                emoji={randomEmoji} 
+                size={60} 
+                style={styles.iconCircle}
+                backgroundColor={randomColour}
+                />
             <ThemedText type="subtitle" style={styles.subtitle} > Better Together </ThemedText>
             <ThemedText type="defaultSemiBold" > Create shared shopping lists and collaborate in real time with family and friends </ThemedText>
         </BodyScrollView>
