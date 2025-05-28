01b8f27b9f7a63db75ed458b635daa046b177624
Jenish <jpat534@gmail.com>
2025-05-28 18:46:16 +1000

    Imported styles and adding a view and text input to the createList component

--- client/app/(index)/list/new/create.tsx
+++ client/app/(index)/list/new/create.tsx
@@ -1,10 +1,96 @@
 import { ThemedText } from "@/components/ThemedText";
 import { BodyScrollView } from "@/components/ui/BodyScrollView";
+import TextInput from "@/components/ui/text-input";
+import { appleBlue } from "@/constants/Colors";
+import { Stack } from "expo-router";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
 export default function CreateList() {
+
+    const [listName, setListName] = useState("");
+
+    const handleCreateList = () => {
+
+    }
+
     return (
-        < BodyScrollView >
-            <ThemedText> Create List </ThemedText>
-        </BodyScrollView>
+
+        <>
+        <Stack.Screen 
+            options={{
+                headerTitle: "New List",
+                headerLargeTitle: false
+            }}
+        />
+            < BodyScrollView contentContainerStyle={styles.scrollViewContent}>
+                <View>
+                    <TextInput placeholder="Grocery Essentials"
+                        size="lg" 
+                        variant="ghost"
+                        value={listName}
+                        onChangeText={setListName}
+                        onSubmitEditing={() => handleCreateList()}
+                        returnKeyType="done"
+                        autoFocus
+                        inputStyle={styles.titleInput}
+                        containerStyle={styles.titleInputContainer}
+                    />
+
+                </View>
+                <ThemedText> Create List </ThemedText>
+            </BodyScrollView>
+        </>
     )
 }
+
+const styles = StyleSheet.create({
+  scrollViewContent: {
+    padding: 16,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  titleInput: {
+    fontWeight: "600",
+    fontSize: 28,
+    padding: 0,
+  },
+  titleInputContainer: {
+    flexGrow: 1,
+    flexShrink: 1,
+    maxWidth: "auto",
+    marginBottom: 0,
+  },
+  emojiButton: {
+    padding: 1,
+    borderWidth: 3,
+    borderRadius: 100,
+  },
+  emojiContainer: {
+    width: 28,
+    height: 28,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  descriptionInput: {
+    padding: 0,
+  },
+  createButtonText: {
+    color: appleBlue,
+    fontWeight: "normal",
+  },
+  colorButton: {
+    padding: 1,
+    borderWidth: 3,
+    borderRadius: 100,
+  },
+  colorContainer: {
+    width: 28,
+    height: 28,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
