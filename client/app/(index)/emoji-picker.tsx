18928f37288b5b04da73b25acba30715dea7b66d
Jenish <jpat534@gmail.com>
2025-05-30 17:16:42 +1000

    Added styles to the emoji picker

--- client/app/(index)/emoji-picker.tsx
+++ client/app/(index)/emoji-picker.tsx
@@ -1,7 +1,7 @@
 import { emojies } from "@/constants/Colors";
 import { useListCreation } from "@/context/ListCreationContext";
 import { useRouter } from "expo-router";
-import { FlatList, Pressable, Text } from "react-native";
+import { FlatList, Pressable, Text, StyleSheet } from "react-native";
 
 export default function EmojiPickerScreen(){
 
@@ -18,9 +18,30 @@ export default function EmojiPickerScreen(){
             data={emojies}
             renderItem={({ item }) => (
                 <Pressable onPress={() => handleEmojiSelect(item)} >
-                    <Text> {item} </Text>
+                    <Text style={styles.emojiText} > {item} </Text>
                 </Pressable>
             ) }
+            numColumns={5}
+            keyExtractor={(item) => item}
+            automaticallyAdjustContentInsets
+            contentInsetAdjustmentBehavior="automatic"
+            contentContainerStyle={styles.contentContainerStyle}
         />
     )
-}
+};
+
+const styles = StyleSheet.create({
+    emojiText: {
+        fontSize: 40,
+    },
+    contentContainerStyle: {
+        padding: 16, 
+        paddingBottom: 100,
+    },
+    pressable: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    }
+})
+
