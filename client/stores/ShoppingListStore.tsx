89faedf2cacd17dbf7358a3c3922374046cb56f2
Jenish <jpat534@gmail.com>
2025-05-31 14:51:55 +1000

    Addd a ShoppingListStore file with the schemas for the data

---
+++ client/stores/ShoppingListStore.tsx
@@ -0,0 +1,29 @@
+const STORE_ID_PREFIX = "shoppingListStore-";
+
+const VALUES_SCHEMA = {
+  listId: { type: "string" },
+  name: { type: "string" },
+  description: { type: "string" },
+  emoji: { type: "string" },
+  color: { type: "string" },
+  createdAt: { type: "string" },
+  updatedAt: { type: "string" },
+} as const;
+
+const TABLES_SCHEMA = {
+  products: {
+    id: { type: "string" },
+    name: { type: "string" },
+    quantity: { type: "number" },
+    units: { type: "string" },
+    isPurchased: { type: "boolean", default: false },
+    category: { type: "string", default: "" },
+    notes: { type: "string" },
+    createdBy: { type: "string" }, // userId
+    createdAt: { type: "string" },
+    updatedAt: { type: "string" },
+  },
+  collaborators: {
+    nickname: { type: "string" },
+  },
+} as const;
